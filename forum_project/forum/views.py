83bd244c98b1eeaef8b28c886728bf1b77ce7754
TheLastSpoonbender <krzysztof.rudnicki678@gmail.com>
2025-01-04 17:50:14 +0100

    added siple views for home and thread pages

--- forum_project/forum/views.py
+++ forum_project/forum/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Thread, Reply
 
-# Create your views here.
+def home(request):
+    categories = Category.objects.all()
+    return render(request, 'forum/home.html', {'categories': categories})
+
+def thread_detail(request, thread_id):
+    thread = get_object_or_404(Thread, id=thread_id)
+    return render(request, 'forum/thread_detail.html', {'thread': thread})
