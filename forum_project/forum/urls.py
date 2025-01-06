a718a163b70abeaf6b0c09160c2f1f2942b5e0d9
TheLastSpoonbender <krzysztof.rudnicki678@gmail.com>
2025-01-06 13:51:28 +0100

    Add functionality to create threads and replies

--- forum_project/forum/urls.py
+++ forum_project/forum/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('thread/<int:thread_id>/', views.thread_detail, name='thread_detail'),
+    path('thread/new/', views.create_thread, name='create_thread'),
+    path('thread/<int:thread_id>/reply/', views.add_reply, name='add_reply'),
 ]
